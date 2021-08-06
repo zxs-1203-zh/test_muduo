c1525e7990ec041483537fa712a38ad0c57718f1
zxs-1203-zh <15021193371@163com>
2021-08-06 21:21:19 +0600

    test_muduo

---
+++ reactor/s05/Acceptor.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <functional>
+
+#include "Socket.h"
+#include "Channel.h"
+#include "InetAddress.h"
+#include "EventLoop.h"
+
+namespace muduo
+{
+
+class Acceptor//noncopyable
+{
+
+public:
+
+	typedef std::function<void(int connFd, InetAddress peerAddr)>
+		    NewConnectionCallback;
+
+	Acceptor(const Acceptor&) = delete;
+	Acceptor& operator=(const Acceptor&) = delete;
+
+	Acceptor(EventLoop* loop, const InetAddress& localAddr);
+
+	void listen();
+
+	bool listenning()const
+	{
+		return listenning_;
+	}
+
+	void setNewConnectionCallback(const NewConnectionCallback& cb)
+	{
+		newConnectionCallback_ = cb;
+	}
+
+private:
+	void handleRead();
+
+	EventLoop* 			  loop_;
+	Socket     			  acceptSocket_;
+	Channel    			  acceptChannel_;
+	bool 				  listenning_;
+	NewConnectionCallback newConnectionCallback_;
+
+};//Acceptor
+
+}//muduo
---
+++ reactor/s05/SocketsOps.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <endian.h>
+#include <netinet/in.h>
+
+namespace muduo
+{
+
+namespace sockets
+{
+
+inline uint16_t hostToNetwork16(uint16_t host)
+{
+	return ::htons(host);
+}
+
+inline uint32_t hostToNetwork32(uint32_t host)
+{
+	return ::htonl(host);
+}
+
+inline uint64_t hostToNetwork64(uint64_t host)
+{
+	return ::htobe64(host);
+}
+
+inline uint16_t networkToHost16(uint16_t net)
+{
+	return ::ntohs(net);
+}
+
+inline uint32_t networkToHost32(uint32_t net)
+{
+	return ::ntohl(net);
+}
+
+inline uint64_t networkToHost64(uint64_t net)
+{
+	return ::be64toh(net);
+}
+
+int createNonblockOrDie();
+
+void bindOrDie(int sockFd, const struct sockaddr_in& addr);
+
+void listenOrDie(int sockFd);
+
+int accept(int sockFd, struct sockaddr_in* addr);
+
+void close(int sockFd);
+
+void fromHostPort(const char* ip, uint16_t port, struct sockaddr_in* addr);
+
+void toHostPort(char* buf, size_t size, const struct sockaddr_in& addr);
+
+}//sockets
+
+}//muduo
